f2cab4379e56d46987b304cec6937da70415695c
yaroslavu4 <yaroslav.no.ctrax@gmail.com>
2023-05-23 12:17:28 +0100

    if

--- Home_work_lecture_3.py
+++ Home_work_lecture_3.py
@@ -5,24 +5,26 @@ print('Task 1')
 # Слово та номер символу отримайте за допомогою input() або скористайтеся константою. Наприклад (слово - "Python" а
 # номер символу 3) - "The 3 symbol in 'Python' is 't' ".
 
-string = ''
-while not string:
-    string = input('Please enter text here: ')
+# мучался над этой задачей,ибо метод isdigit() не распознает знак минус, пришлось делить цикл на две части
+# мне не нравится как сильно похожи обе части цикла, но по-другому я сделать не смог(
+string = input('Please enter text here: ')
 symbol = input('Enter symbol number: ')
 if symbol[0] == '-':
     if symbol[1:].isdigit():
+        int_symbol = int(symbol)
         if int(symbol) < -len(string):
             print('Symbol number is out of string, try again!')
         else:
-            print(f'The symbol {int(symbol)} in {string} is "{string[int(symbol)]}"')
+            print(f'The symbol {int_symbol} in {string} is "{string[int_symbol]}"')
     else:
         print('Use digits only!')
 else:
     if symbol.isdigit():
+        int_symbol = int(symbol)
         if int(symbol) >= len(string):
             print('Symbol number is out of string, try again!')
         else:
-            print(f'The symbol {int(symbol)} in {string} is "{string[int(symbol)]}"')
+            print(f'The symbol {int_symbol} in {string} is "{string[int_symbol]}"')
     else:
         print('Use digits only!')
 
@@ -32,7 +34,7 @@ print('Task 2')
 # Вести з консолі строку зі слів за допомогою input() (або скористайтеся константою).
 # Напишіть код, який визначить кількість слів, в цих даних.
 
-string = input('enter a sentence: ')
+string = input('Enter a sentence: ')
 my_list = string.split()
 print(f'There is {len(my_list)} words in string "{string}" ')
 print('\n')
@@ -45,6 +47,8 @@ print("Task 3")
 # Напишіть код, який сформує новий list (наприклад lst2), який би містив всі числові змінні (int, float), які є в lst1.
 # Майте на увазі, що данні в lst1 не є статичними можуть змінюватись від запуску до запуску.
 
-lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
+lst = ['1', '2', 3, 4.0, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
+new_lst = [value for value in lst if type(value) == int or type(value) == float]
+print(new_lst)
 
 
