c96bf7692ede4359d4d385252e455a402d06735d
yaroslavu4 <yaroslav.no.ctrax@gmail.com>
2023-05-26 23:18:30 +0100

    if

---
+++ Home_work_lecture_4.py
@@ -0,0 +1,50 @@
+print('Task 1')
+# Task 1
+# Дана довільна строка. Напишіть код, який знайде в ній і віведе на екран кількість слів,
+# які містять дві голосні літери підряд.
+
+lst = ''
+while not lst:
+    lst = input('Enter a string: ').lower().split()
+
+vowels = ['а', 'е', 'и', 'і', 'о', 'у', 'я', 'ю', 'є', 'ї']
+vowels_list = []
+count = 0
+
+if not ''.join(lst).isalpha():
+    print('Use letters, not digits!')
+else:
+    for word in lst:    # ["Яярик", "ты", "крУутой"]
+        for letter in word:
+            count = count + 1 if letter in vowels else 0
+            # if letter in vowels:
+            #     count += 1
+            # else:
+            #     count = 0
+            if count == 2:
+                vowels_list.append(word)
+                break
+    print(f'There are {len(vowels_list)} words with repetitive vowels')
+print('\n')
+
+
+print('Task 2')
+# Є два довільних числа які відповідають за мінімальну і максимальну ціну. Є Dict з назвами магазинів і цінами:
+# {
+#   "cito": 47.999,
+#   "BB_studio": 42.999,
+#   "momo": 49.999,
+#   "main-service": 37.245,
+#   "buy.now": 38.324,
+#   "x-store": 37.166,
+#   "the_partner": 38.988,
+#   "store": 37.720,
+#   "rozetka": 38.003
+# }
+# Напишіть код, який знайде і виведе на екран назви
+# магазинів, ціни яких попадають в діапазон між мінімальною і максимальною ціною. Наприклад:
+# lower_limit = 35.9
+# upper_limit = 37.339
+# > match: "x-store", "main-service"
+
+
